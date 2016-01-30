52f3a78bffc74c78baac53bdd6c3e0be8c7d91f1
the6p4c <me@doggirl.gay>
2016-01-30 12:36:13 +1000

    Add charts by day and by hour

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
+var statistics = [
+	// Listening by day
+	function(data) {
+		var chartData = [0, 0, 0, 0, 0, 0, 0];
+		for (var i = 0; i < data.length; ++i) {
+			var row = data[i];
+
+			++chartData[row.timestamp.getDay()];
+		}
+
+		chartData.push(chartData.shift());
+
+		var ctx = $("#chart-byDay").get(0).getContext("2d");
+		var chart = new Chart(ctx).Bar({
+			labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+			datasets: [
+				{
+					data: chartData
+				}
+			]
+		});
+	},
+
+	function(data) {
+		var chartData = [];
+		var labels = [];
+
+		for (var i = 0; i < 24; ++i) {
+			chartData.push(0);
+			var paddedHr = i < 10 ? "0" + i : i;
+			labels.push(paddedHr + ":00");
+		}
+
+		for (var i = 0; i < data.length; ++i) {
+			var row = data[i];
+
+			++chartData[row.timestamp.getHours()];
+		}
+
+		var ctx = $("#chart-byHour").get(0).getContext("2d");
+		var chart = new Chart(ctx).Bar({
+			labels: labels,
+			datasets: [
+				{
+					data: chartData
+				}
+			]
+		});
+	}
+];
+
 function parseStringTimestamp(timestampString) {
 	return new Date(timestampString + " GMT+0000");
 }
@@ -41,7 +92,12 @@ $(document).ready(function() {
 
 				var data = dataTransform(csvData);
 
-				console.log(data);
+				$("#load-file-section").hide();
+				$("#stats").show();
+
+				for (var i = 0; i < statistics.length; ++i) {
+					statistics[i](data);
+				}
 			};
 
 			fileReader.readAsText(file);
